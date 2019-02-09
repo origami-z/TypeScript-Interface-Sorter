6e4d2d6d9df8585f14a88b9a768441f67286a843
Zhihao Cui <zhihaocui@Zhihaos-MacBook-Pro-2018.local>
2019-02-09 16:13:13 +0000

    Add more test cases

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -3,19 +3,47 @@ import * as vscode from "vscode";
 
 import { SimpleTsParser } from "../components/parser";
 
-import { tcClassImplementInterface, tcEmptyInterface } from "./testCases";
+import {
+  tcClassImplementInterface,
+  tcEmptyInterface,
+  tcPrefixExport,
+  tcInterfaceWithOneProperty,
+  tcInterfaceWithExtends,
+  tcInterfaceWithComment
+} from "./testCases";
 
 describe("Parser", () => {
   const parser = new SimpleTsParser();
   const filePath = "Untitled-1";
-  describe("parse correct number of interface", () => {
+  describe("parse number of interface", () => {
     test("should parse no interface", () => {
       const { nodes } = parser.parseInterface(filePath, tcClassImplementInterface);
       expect(nodes.length).toBe(0);
     });
 
-    test("should pass one interface with input of empty interface", () => {
+    test("should parse one interface with input of empty interface", () => {
       const { nodes } = parser.parseInterface(filePath, tcEmptyInterface);
+      expect(nodes.length).toBe(1);
+    });
+
+    test("should parse one interface with export prefix", () => {
+      const { nodes } = parser.parseInterface(
+        filePath,
+        tcPrefixExport + tcInterfaceWithOneProperty
+      );
+      expect(nodes.length).toBe(1);
+    });
+
+    test("should parse two interfaces with one extends the other", () => {
+      const { nodes } = parser.parseInterface(
+        filePath,
+        `${tcEmptyInterface}\n\n${tcInterfaceWithExtends}`
+      );
+      expect(nodes.length).toBe(2);
+    });
+
+    test("should parse one interface with comments", () => {
+      const { nodes } = parser.parseInterface(filePath, tcInterfaceWithComment);
       expect(nodes.length).toBe(1);
     });
   });
