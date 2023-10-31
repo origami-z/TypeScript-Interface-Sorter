5aa0a6967000b0167ebc8e26040cd187a2841ae2
origami-z <davidcui2@me.com>
2023-10-31 17:40:15 +0000

    Take `editor.tabSize` into account

--- src/test/components/sorter.test.ts
+++ src/test/components/sorter.test.ts
@@ -20,8 +20,9 @@ import {
 } from "./test-cases";
 
 describe("Sorter", () => {
-
-  const parser = new SimpleTsParser(new SimpleConfigurator({ default: defaultConfig }));
+  const parser = new SimpleTsParser(
+    new SimpleConfigurator({ default: defaultConfig })
+  );
   const sorter = new SimpleTsSorter(
     new SimpleConfigurator({ default: defaultConfig })
   );
@@ -45,19 +46,25 @@ describe("Sorter", () => {
     expect(sorted.length).toBe(0);
   });
 
-  test.each([tcPrefixes])("should sort one with export prefix - %s", (prefix) => {
-    const prefixLength = prefix.length;
-    const exportLength = tcPrefixExport.length;
-    const { nodes } = parser.parseTypeNodes(
-      filePath,
-      tcPrefixExport + prefix + typeWithOneProperty
-    );
-    const sorted = sorter.sortGenericTypeElements(nodes);
+  test.each([tcPrefixes])(
+    "should sort one with export prefix - %s",
+    (prefix) => {
+      const prefixLength = prefix.length;
+      const exportLength = tcPrefixExport.length;
+      const { nodes } = parser.parseTypeNodes(
+        filePath,
+        tcPrefixExport + prefix + typeWithOneProperty
+      );
+      const sorted = sorter.sortGenericTypeElements(nodes);
 
-    expect(sorted.length).toBe(1);
-    //  
-    expect(sorted[0].rangeToRemove).toEqual({ pos: prefixLength + exportLength + 1, end: prefixLength + exportLength + 17 });
-  });
+      expect(sorted.length).toBe(1);
+      //
+      expect(sorted[0].rangeToRemove).toEqual({
+        pos: prefixLength + exportLength + 1,
+        end: prefixLength + exportLength + 17,
+      });
+    }
+  );
 
   test("should sort two interfaces with one extends the other", () => {
     const textIntput = tcInterfaceWithExtends;
@@ -78,9 +85,14 @@ describe("Sorter", () => {
     const sorted = sorter.sortGenericTypeElements(nodes);
 
     expect(sorted.length).toBe(1);
-    expect(sorted[0].rangeToRemove).toEqual({ pos: prefixLength + 1, end: prefixLength + 121 });
+    expect(sorted[0].rangeToRemove).toEqual({
+      pos: prefixLength + 1,
+      end: prefixLength + 121,
+    });
     expect(sorted[0].textToReplace).toEqual(
-      textIntput.substring(prefixLength + 71, prefixLength + 121) + "\n" + textIntput.substring(prefixLength + 1, prefixLength + 70)
+      textIntput.substring(prefixLength + 71, prefixLength + 121) +
+        "\n" +
+        textIntput.substring(prefixLength + 1, prefixLength + 70)
     );
   });
 
@@ -186,5 +198,23 @@ describe("Sorter", () => {
         "\n  RequiredProp2: string;\n\n  requiredProp1: string;\n\n  OptionalProp2?: string;\n\n  optionalProp1?: string;"
       );
     });
+  });
+
+  test("should take `indentSpace` setting into account", () => {
+    const sorter2 = new SimpleTsSorter(
+      new SimpleConfigurator({
+        default: { ...defaultConfig, indentSpace: 4 },
+      })
+    );
+
+    const textIntput = tcInterfaceWithCapitalLetter;
+    const { nodes } = parser.parseTypeNodes(filePath, textIntput);
+    const sorted = sorter2.sortGenericTypeElements(nodes);
+
+    expect(sorted.length).toBe(1);
+    expect(sorted[0].rangeToRemove).toEqual({ pos: 23, end: 82 });
+    expect(sorted[0].textToReplace).toEqual(
+      "\n    description: string;\n\n    Length: number;\n\n    name: string;"
+    );
   });
 });
